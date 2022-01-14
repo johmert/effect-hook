53702274a07f61a90650b22aa2d215f77b7d0299
johmert <johmert@users.noreply.github.com>
2022-01-14 15:27:05 -0800

    effect hook w/ cleanup setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, { useState } from "react";
 import ProfileEdit from './ProfileEdit';
 import './App.css';
 
 function App() {
+  const [userId, setUserID] = useState(1);
+
+  const userIds = [1, 2, 3, 4];
+
   return (
     <div className="App">
-      <ProfileEdit />
+      {userIds.map((id) => (
+        <button key={id} onClick={() => setUserID(id)}>
+          User ID {id}
+        </button>
+      ))}
+      <h2>User ID {userId}</h2>
+      <ProfileEdit userID={userId} />
     </div>
   );
 }
